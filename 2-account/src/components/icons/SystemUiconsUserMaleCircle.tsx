e27e70c5667e9979d5008a3c462f78cb29187e6c
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2025-10-04 17:19:41 +0330

    added account folder

---
+++ 2-account/src/components/icons/SystemUiconsUserMaleCircle.tsx
@@ -0,0 +1,8 @@
+import React, { SVGProps } from 'react'
+
+export function SystemUiconsUserMaleCircle(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 21 21" {...props}>{/* Icon from System UIcons by Corey Ginnivan - https://github.com/CoreyGinnivan/system-uicons/blob/master/LICENSE */}<g fill="none" fillRule="evenodd" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" transform="translate(2 2)"><circle cx="8.5" cy="8.5" r="8" /><path d="M14.5 13.5c-.662-2.274-3.2-3.025-6-3.025c-2.727 0-5.27.869-6 3.025" /><path d="M8.5 2.5a3 3 0 0 1 3 3v2a3 3 0 0 1-6 0v-2a3 3 0 0 1 3-3" /></g></svg>
+  )
+}
+export default SystemUiconsUserMaleCircle
