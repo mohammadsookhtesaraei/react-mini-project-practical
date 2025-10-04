81c20801c4ffa457713317f6f4045314cc7e2581
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2025-10-04 16:59:52 +0330

    finish

--- 1-darkmode/src/App.tsx
+++ 1-darkmode/src/App.tsx
@@ -1,8 +1,9 @@
+import Header from "./components/Header"
 
 
 const App = () => {
   return (
-    <div>App</div>
+    <Header/>
   )
 }
 
--- 1-darkmode/src/components/Header.tsx
+++ 1-darkmode/src/components/Header.tsx
@@ -1,13 +1,44 @@
-
-
-
-
-
+import { useEffect, useState } from "react";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const [theme, setTheme] = useState(() => {
+    if (typeof window === "undefined") return "light";
+    const saved = localStorage.getItem("theme");
+    if (saved) return saved;
+    const systemPrefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    return systemPrefersDark ? "dark" : "light";
+  });
 
-export default Header
+  useEffect(() => {
+    const htmlElement = document.documentElement;
+    htmlElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+  return (
+    <div className=" flex items-center font-sans  shadow shadow-white  justify-between py-5 px-5 bg-gray-300 text-black dark:bg-slate-800 dark:text-gray-200">
+      <nav>
+        <ul className="flex gap-4 font-medium capitalize">
+          <li>home</li>
+          <li>aboutUs</li>
+          <li>contact us</li>
+        </ul>
+      </nav>
+      <div>
+        <button
+          onClick={toggleTheme}
+          className="cursor-pointer transition-all delay-150 ease-in-out"
+        >
+          {theme === "dark" ? "Switch to Light 🌞" : "Switch to Dark 🌙"}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
